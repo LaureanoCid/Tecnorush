83b809b7f9ba68b327259d633716a7e54651246f
Laureano Cid <laurycid@gmail.com>
2024-10-16 01:52:49 -0300

    Primera Pre-Entrega

--- js/productos.js
+++ js/productos.js
@@ -14,7 +14,7 @@ let monto = 0;
 alert("Bienvenid@ al carro de compras de TECNORUSH!");
 
 // Login con su respectivas validaciones
-login();
+//login();
 
 // MENU del carrito
 do {
@@ -65,6 +65,8 @@ function comprarProductos(cant, lista, monto) {
     let op_prod = 0;
 
     switch (op) {
+
+        //Compra para -- GABINETES 
         case 1:
             op_prod = parseInt(prompt("Presione la opcion deseada: \n1. Gabinete Deepcool - $109.000 \n2. Gabinete Thermaltake - $163.000 \n3. Gabinete Corsair - $210.000 "));
             switch (op_prod) {
@@ -91,14 +93,68 @@ function comprarProductos(cant, lista, monto) {
                     break;
             }
             break;
+
+        //Compra para -- PROCESADORES 
+        case 2:
+            op_prod = parseInt(prompt("Presione la opcion deseada: \n1. Ryzen 7 5700G - $325.000 \n2. Ryzen 5 5600X - $215.000 \n3. Ryzen 3 3200G - $130.000 "));
+            switch (op_prod) {
+                case 1:
+                    alert("Ryzen 7 5700G agregado a su carrito");
+                    cant++;
+                    monto += 325.000;
+                    lista += "\n" + cant + " Ryzen 7 5700G - $325.000";
+                    break;
+                case 2:
+                    alert("Ryzen 5 5600X agregado a su carrito");
+                    cant++;
+                    monto += 215.000;
+                    lista += "\n" + cant + " Ryzen 5 5600X - $215.000";
+                    break;
+                case 3:
+                    alert("Ryzen 3 3200G agregado a su carrito");
+                    cant++;
+                    monto += 130.000;
+                    lista += "\n" + cant + " Ryzen 3 3200G - $130.000";
+                    break;
+                default:
+                    alert("Opcion incorrecta");
+                    break;
+            }
+            break;
+
+        //Compra para -- AURICULARES 
+        case 3:
+            op_prod = parseInt(prompt("Presione la opcion deseada: \n1. Razer Blackshark V2 Pro - $645.000 \n2. JBL 910 RGB - $630.000 \n3. Logitech G G733 - $460.000 "));
+            switch (op_prod) {
+                case 1:
+                    alert("Razer Blackshark V2 Pro agregado a su carrito");
+                    cant++;
+                    monto += 645.000;
+                    lista += "\n" + cant + " Razer Blackshark V2 Pro - $645.000";
+                    break;
+                case 2:
+                    alert("JBL 910 RGB agregado a su carrito");
+                    cant++;
+                    monto += 630.000;
+                    lista += "\n" + cant + " JBL 910 RGB - $630.000";
+                    break;
+                case 3:
+                    alert("Logitech G G733 agregado a su carrito");
+                    cant++;
+                    monto += 460.000;
+                    lista += "\n" + cant + " Logitech G G733 - $460.000";
+                    break;
+                default:
+                    alert("Opcion incorrecta");
+                    break;
+            }
+            break;
+
         default:
             alert("Opcion incorrecta");
             break;
     }
     alert(lista);
-    console.log(cant);
-    console.log(monto);
-    console.log(lista);
 }
 
 
